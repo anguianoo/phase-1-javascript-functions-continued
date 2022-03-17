ea9204989dfdf1c33c3cc88b2b3478792faed1dd
anguianoo <njanguiano@gmail.com>
2022-03-17 15:41:43 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(string = "roller-skate") {
+  console.log(`"This Saturday, I want to ${string}!"`);
+}
+
+// function mondayWork(string) {
+//   if (string != "") {
+//     console.log(`This Monday, I will ${string}`);
+//   } else console.log("This Monday, I will go to the office");
+// }
+
+let mondayWork = function (string = "go to the office") {
+  return `this Monday, I will ${string}`;
+};
+
+let wrapAdjective = function (style = "*") {
+  return function (string = "special") {
+    return `You are ${style}${string}${style}!`;
+  };
+};
